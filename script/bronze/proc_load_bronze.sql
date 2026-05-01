bc797b202819f8484a5afc3129eeb0910893faa0
Shehzad Hussain <shahzadhussainalvi760@gmail.com>
2026-05-01 16:42:35 +0500

    Create proc_load_bronze.sql

---
+++ script/bronze/proc_load_bronze.sql
@@ -0,0 +1,160 @@
+
+/*
+====================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+====================================================================
+
+Script Purpose:
+This stored procedure loads data into the 'bronze' schema from external CSV files.
+It performs the following actions:
+- Truncates the bronze tables before loading data.
+- Uses the 'BULK INSERT' command to load data from csv files to bronze tables.
+
+Parameters:
+None.
+
+This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+EXEC bronze.load_bronze;
+
+====================================================================
+*/
+EXEC bronze.load_bronze ;
+
+CREATE OR ALTER PROCEDURE bronze.load_bronze
+AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME
+    BEGIN TRY
+        SET @batch_start_time = GETDATE()
+        PRINT '======================================================================';
+        PRINT 'Loading Bronze Layer ';
+        PRINT '======================================================================';
+
+        PRINT'-----------------------------------------------------------------------';
+        PRINT'Loading CRM TABELS ';
+        PRINT'-----------------------------------------------------------------------';
+        
+        SET @start_time = GETDATE();
+        PRINT'>> Truncate Table: bronze.crm_cust_info '
+        TRUNCATE TABLE bronze.crm_cust_info  ;
+
+        PRINT '>> Inserting data into : bronze.crm_cust_info'
+        BULK INSERT bronze.crm_cust_info
+        FROM  'C:\Users\aaa\OneDrive\Documents\SQL File\SQL Full Course for Beginners (30 Hours) – From Zero to Hero\Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        );
+
+        SELECT COUNT(*) FROM bronze.crm_cust_info ;
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration :' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) +'second';
+        PRINT '-------------------------------------------------------------------------------';
+        --
+        SET @start_time= GETDATE()
+        PRINT'>> Trancate table :bronze.crm_prd_info'
+        TRUNCATE TABLE bronze.crm_prd_info;
+        PRINT'>>Inserting data into : bronze.crm_prd_info'
+        BULK INSERT bronze.crm_prd_info
+        FROM 'C:\Users\aaa\OneDrive\Documents\SQL File\SQL Full Course for Beginners (30 Hours) – From Zero to Hero\Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+        )
+        SELECT * FROM bronze.crm_prd_info;
+        SET @end_time= GETDATE()
+        PRINT'>> Loading Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+ 'seconds'
+        PRINT'-----------------------------------------------------------------------'
+        ---
+        SET @start_time = GETDATE()
+        PRINT'>> Trancate table : bronze.crm_sales_details'
+        TRUNCATE TABLE bronze.crm_sales_details;
+        PRINT '>> Inserting data into : bronze.crm_sales_details '
+        BULK INSERT bronze.crm_sales_details 
+        FROM 'C:\Users\aaa\OneDrive\Documents\SQL File\SQL Full Course for Beginners (30 Hours) – From Zero to Hero\Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR= ',',
+        TABLOCK
+        );
+
+        SELECT * FROM bronze.crm_sales_details;
+        SET @end_time = GETDATE()
+        PRINT'>> Loading Duration '+ CAST(DATEDIFF(SECOND,@start_time ,@end_time) AS NVARCHAR) +'seconds'
+        PRINT'--------------------------------------------------------------------';
+
+        PRINT'-----------------------------------------------------------------------';
+        PRINT'Loading ERM TABELS ';
+        PRINT'-----------------------------------------------------------------------';
+        SET @start_time = GETDATE()
+        PRINT 'Trancte table : bronze.erp_cust_az12 ';
+        TRUNCATE TABLE bronze.erp_cust_az12 ;
+
+        PRINT'>> Inserting data into :bronze.erp_cust_az12 ';
+        BULK INSERT bronze.erp_cust_az12 
+        FROM 'C:\Users\aaa\OneDrive\Documents\SQL File\SQL Full Course for Beginners (30 Hours) – From Zero to Hero\Project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+        WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR= ',',
+        TABLOCK
+        );
+        SELECT COUNT(*) FROM bronze.erp_cust_az12 ;
+        SET @end_time= GETDATE()
+        PRINT '>> Loading Duration:'+ CAST(DATEDIFF(SECOND,@start_time ,@end_time) AS NVARCHAR)+'seconds'
+        PRINT '----------------------------------------------------------------------------'
+        
+        SET @start_time = GETDATE()
+        PRINT'>>> Trancte table : bronze.erp_loc_a101';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+
+        PRINT'Inserting data into :bronze.erp_loc_a101';
+        BULK INSERT bronze.erp_loc_a101
+        FROM 'C:\Users\aaa\OneDrive\Documents\SQL File\SQL Full Course for Beginners (30 Hours) – From Zero to Hero\Project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
+            );
+        SELECT * FROM bronze.erp_loc_a101;
+        SET @end_time = GETDATE()
+        PRINT '>> Loading Duration :' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+        PRINT'----------------------------------------------------------------'
+
+        ---
+        SET @start_time = GETDATE()
+        PRINT 'Trancate tabel : bronze.erp_px_cat_g1v2'
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        PRINT'Inserting data into : bronze.erp_px_cat_g1v2'
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM 'C:\Users\aaa\OneDrive\Documents\SQL File\SQL Full Course for Beginners (30 Hours) – From Zero to Hero\Project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+        WITH(
+        FIRSTROW= 2,
+        FIELDTERMINATOR= ',',
+        TABLOCK)
+        SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2;
+        SELECT * FROM bronze.erp_px_cat_g1v2;
+
+        SET @end_time = GETDATE()
+        PRINT'Loading Duration :' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR) + 'second';
+        PRINT'-------------------------------------------------------------'
+
+        SET @batch_end_time = GETDATE()
+        PRINT'===========================================================';
+        PRINT'Loading Bronze Layer is Complete'
+        PRINT '- Total Loading Duration : '+ CAST(DATEDIFF(SECOND,@batch_start_time,@batch_end_time) AS NVARCHAR) + 'seconds'
+        PRINT'==========================================================='
+     END TRY
+
+     BEGIN CATCH
+     PRINT'========================================================================';
+     PRINT'ERROR OCCUR DURNGING LOADING BRONZE LAYER'
+     PRINT 'Error Message'+ ERROR_MESSAGE();
+     PRINT 'Error Message'+ CAST(ERROR_NUMBER() AS NVARCHAR);
+     PRINT 'Error Message'+ CAST(ERROR_STATE() AS NVARCHAR);
+     END CATCH
+END;
